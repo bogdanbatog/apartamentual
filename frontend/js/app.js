9fb40010be2827dc4b40b7a664e3c538279e8790
lutalucianm-lang <luta.lucian.m@gmail.com>
2026-02-09 00:08:56 +0200

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,4 @@
-// Supabase configuration
-const SUPABASE_URL = 'https://glbvbbgmcobtswwlktic.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_I25cj3p8FZJyTAe0X2ngDA_vvz6ssWz';
+
 
 // Initialize Supabase client
 let supabase;
