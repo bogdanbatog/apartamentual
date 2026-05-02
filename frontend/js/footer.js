baa017c040266372636dfc1484ddaf65a343d1c4
lutalucianm-lang <luta.lucian.m@gmail.com>
2026-05-02 15:44:41 +0300

    Update footer.js

--- frontend/js/footer.js
+++ frontend/js/footer.js
@@ -91,6 +91,7 @@ function createFooter() {
                     <h4>Legal</h4>
                     <a href="/termeni.html">Termeni și condiții</a>
                     <a href="/gdpr.html">GDPR</a>
+                    <a href="/politica-retur.html">Politica de retur</a>
                     <a href="/contact.html#faq-container">FAQ</a>
                     <a href="/ghid.html">Ghid platformă</a>
                     <a href="/contact.html">Contact</a>
