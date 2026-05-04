a6b11cabc0d8e06c9e7258204e442eea5e4acee1
lutalucianm-lang <luta.lucian.m@gmail.com>
2026-05-05 00:01:40 +0300

    Add files via upload

--- frontend/js/footer.js
+++ frontend/js/footer.js
@@ -163,7 +163,7 @@ function createFooter() {
             <div class="site-footer-company">
                 <span><strong>SC LTFB Studio SRL</strong></span>
                 <span>CUI: <strong>RO22004992</strong></span>
-                <span>Reg. Com.: <strong>J40/12417/2007</strong></span>
+                <span>Reg. Com.: <strong>J2007012417402</strong></span>
                 <span>Sediu: Str. Popa Petre 23, Sector 2, București</span>
                 <span>Telefon: <a href="tel:+40723870834">+40 723 870 834</a></span>
                 <span>Email: <a href="mailto:office@ltfbstudio.ro">office@ltfbstudio.ro</a></span>
